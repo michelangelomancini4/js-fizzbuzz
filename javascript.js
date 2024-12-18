ca0cfe7e965593016d594335c1b792d7fd9e9e5f
michelangelomancini <michelangelomancini8@gmail.com>
2024-12-18 14:14:26 +0100

    program done

--- javascript.js
+++ javascript.js
@@ -1,19 +1,26 @@
+let text = "il valore di i è: ";
+
 // Scrivi un programma che stampi i numeri da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    console.log("il valore di i è: ", i);
-    // per i multipli di 3 stampi “Fizz” al posto del numero
-    if (i % 3 === 0){
-        console.log ("il valore di i è: ", "Fizz");
+        
+ //  Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz come valore di i
+
+if  (i % 3 === 0 && i % 5 === 0) { 
+    console.log(text, "FizzBuzz");
     }
-    // per i multipli di 5 stampi Buzz
-    else if (i % 5 === 0){
-        console.log("il valore di i è: ", "Buzz");
-    }
-   // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
-     if (i % 3 === 0 && i % 5 === 0) { 
-        console.log("il valore di i è: ", "FizzBuzz");
-    }
-    
+        
+    // per i multipli di 3 stampi “Fizz” come valore di i
+else  if (i % 3 === 0){
+    console.log (text, "Fizz") ;
+}
+// per i multipli di 5 stampi Buzz
+else if (i % 5 === 0){
+    console.log(text, "Buzz");
+}
+
+else {
+    console.log(text, i);
+}
 
 }
 
